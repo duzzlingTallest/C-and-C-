1fe4f849d648994ba878c6036fd05f0ceb9f9271
kaushal damani <kaushaldamani386@gmail.com>
2022-12-16 09:11:36 +0530

    initial project

---
+++ c-and-c---project-master/PA-1.c
@@ -0,0 +1,14 @@
+/* 1. Display This Information using printf 
+a. Your Name 
+b. Your Birth date 
+c. Your Age 
+d. Your Address*/
+#include<stdio.h>
+void main()
+{
+
+printf("NAME : Kaushal K. Damani\n");
+printf("BIRTHDATE : 18TH jun,2004\n");
+printf("AGE : 19 YEAR OLD\n");
+printf("ADDRESS : 31,Nillhanth Homes,Kosamada,SURAT-395006\n");
+}
